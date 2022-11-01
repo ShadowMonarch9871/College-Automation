e95e2d5b25d286b8d8fef5b95673ff9fc2b516dd
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2022-11-01 14:27:23 +0530

    Selector added

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -3,37 +3,62 @@ import React from "react";
 export default function form() {
   return (
     <>
-      <form className="border shadow p-3 rounded">
-        <div className ="mb-3">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <input type="text" className="form-control" id="username" />
-          <div id="emailHelp" className="form-text">
-            Enter your Username here
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input type="text" className="form-control" id="password" />
-        </div>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <form
+          className="border shadow p-3 rounded"
+          style={{
+            width: "400px",
+          }}
+        >
+          <h1 className="text-center p3">LOGIN</h1>
 
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htlmFor="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+            />
+            <div id="emailHelp" className="form-text">
+              Enter your Username here
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="password"
+              name="password"
+            />
+          </div>
+          <select class="form-select mb-3"  aria-label="Default select example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+
+          
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 }
