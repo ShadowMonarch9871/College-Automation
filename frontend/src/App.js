08115e5b62d71540aaca96a112760106aec21fbe
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2022-11-01 10:41:16 +0530

    Home and login page

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Form from "./form";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Form />
+    </>
+  );
+}
+
+export default App;
