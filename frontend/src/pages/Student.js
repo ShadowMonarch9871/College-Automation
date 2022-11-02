251be2ee449e0053392527d4ceca4c0aebe0c561
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2022-11-02 10:25:27 +0530

    Sidebar done

--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
+// import Buttons from"../components/Buttons";
+import Sidebar from '../components/Sidebar';
 
 export default function student() {
   return (
     <>
     <Navbar/>
+    <Sidebar/>
     </>
   );
 }
