9334b12429e1bbc15b2a7fa227e2b2d57f4e7617
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2022-11-02 13:03:21 +0530

    commiting

--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from "../components/Navbar";
 // import Buttons from"../components/Buttons";
 import Sidebar from '../components/Sidebar';
-import MarksTable from"../components/MarksTable";
+// import MarksTable from"../components/MarksTable";
+import Profile from "../components/Profile"
 export default function student() {
   return (
     <>
     <Navbar/>
-    <Sidebar/>
-    <MarksTable/>
+    <Sidebar/> 
+    {/* <MarksTable/> */}
+    {/* <Profile/> */}
     </>
   );
 }
